a6d05ca28d552c74bb3caf535b1da217a5565618
john <jgh2102@gmail.com>
2020-08-18 06:35:46 +0900

    doubled checked with mocha

--- server.js
+++ server.js
@@ -6,31 +6,54 @@ http.createServer(function (req, res) {
         res.writeHead(200);
         res.end();
     }
-    if (req.url === "/Bad-Request") {
-        console.log("Server received your request but could not understand it");
-        res.writeHead(400);
-        res.end();
-    }
-    if (req.url === "/Created") {
+
+    else if (req.url === "/Created") {
         console.log("Your request was received and a new record was created as a result");
         res.writeHead(201);
         res.end();
     }
-    if (req.url === "/Forbidden") {
-        console.log("The resource you requested may exist, but you're not allowed to see it at all");
-        res.writeHead(403);
-        res.end();
+    else if (res.url === '/Moved-Permanently'){
+        console.log('The resource you requested is in a totally new location.');
+        res.writeHead(301);
+        res.end;
     }
-    if (req.url === "/Found") {
+    else if (req.url === "/Found") {
         console.log("This resource has been temporarilary moved");
         res.writeHead(302);
         res.end();
     }
-    else {
-        console.log("Requested page could not be found");
+    else if (req.url === "/Bad-Request") {
+        console.log("Server received your request but could not understand it");
+        res.writeHead(400);
+        res.end();
+    }
+    else if (res.url === '/Unauthorized'){
+        console.log('The resource you requested my exist, but you are not allowed to see it without authentication');
+        res.writeHead(401);
+        res.end;
+    } 
+    else if (req.url === "/Forbidden") {
+        console.log("The resource you requested may exist, but you're not allowed to see it at all");
+        res.writeHead(403);
+        res.end();
+    }
+    else if (req.url ==="/Not-Found"){
+        console.log("Server could not find requested resource.");
         res.writeHead(404);
         res.end();
     }
+    else if (req.url ==="/Internal-Server-Error"){
+        console.log('The request was recieved and the server tried to process it but something went awry');
+        res.writeHead(500);
+        res.end();
+        }
+    
+    else if (req.url === "/Gateway-Timeout"){
+        console.log("The server didn't respond in time.");
+        res.writeHead(504);
+        res.end();
+    }
+    
 }).listen(3000, function () {
     console.log('Welcome to port 3000.');
 });
